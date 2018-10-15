a47808894db9e1f5e61de1f9bc08ffba39790e8c
Quinten van den Herik <99041030@mydavinci.nl>
2018-10-15 15:42:11 +0200

    PokeBattle v0.17
    +added Pokemon moves
    +Pokemons keep fighting untill a Pokemon faints

--- index.php
+++ index.php
@@ -14,11 +14,13 @@ dump($charmeleon);
 PrintPokemonStats($pikachu);
 PrintPokemonStats($charmeleon);
 
-$pikachu->fight($charmeleon);
-$charmeleon->fight($pikachu);
+while($pikachu->hitpoints > 0 & $charmeleon->hitpoints > 0) {
+	$pikachu->fight($charmeleon);
+	$charmeleon->fight($pikachu);
 
-PrintPokemonStats($pikachu);
-PrintPokemonStats($charmeleon);
+	PrintPokemonStats($pikachu);
+	PrintPokemonStats($charmeleon);
+}
 
 function PrintPokemonStats($pokemon) {
 	echo '<div style="border: 1px solid silver; font-weight:bold">';
--- pokestats.php
+++ pokestats.php
@@ -1,7 +1,8 @@
 <?php
 
 $stats = [
-	'Pikachu' => ['defense'=>34, 'attack'=>3, 'moves'=>['Growl'=>45, 'Snipe'=>65]  ],
+	'Pikachu' => ['defense'=>34, 'attack'=>3, 'moves'=>['Quick Attack'=>40, 'Growl'=>45, 'Thunder Shock'=>40, 'Discharge'=>80,]  ],
+	'Charmeleon' => ['defense'=>34, 'attack'=>3, 'moves'=>['Ember'=>40, 'Smokescreen'=>0, 'Dragon Rage'=>0,'Fire Fang'=>65,]  ],
 ];
 
 
