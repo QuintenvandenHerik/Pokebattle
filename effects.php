7608fbeee3e97c31cb795ba51618e8325c201046
Quinten van den Herik <99041030@mydavinci.nl>
2018-10-16 13:40:22 +0200

    PokeBattle v0.18
    +bussy adding status effects for the Pokémons

---
+++ effects.php
@@ -0,0 +1,14 @@
+<?php
+
+$effects = [
+	'Burn' => [/* Damages the target for 1/8 of its HP, and attack is reduced to 50% */],
+	'Confused' => [/* Duration of 1-4 turns, has a 50% chance to attack itself, and attack is reduced to 50% */],
+	'Flinch' => [/* Unable to move for 1 turn */],
+	'frozen' => [/* Unable to move for 1-2 turns, firetype attacks remove this effect */],
+	'Burn' => [/* Damages the target for 1/8 of its HP, and attack is reduced to 50% */],
+	'Burn' => [/* Damages the target for 1/8 of its HP, and attack is reduced to 50% */],
+	'Burn' => [/* Damages the target for 1/8 of its HP, and attack is reduced to 50% */],
+	'Burn' => [/* Damages the target for 1/8 of its HP, and attack is reduced to 50% */],
+]
+
+?>
