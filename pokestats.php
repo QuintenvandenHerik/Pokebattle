eef3d46dac75b248017f264d676582397a6e001b
Quinten van den Herik <99041030@mydavinci.nl>
2018-10-17 14:05:27 +0200

    PokeBattle v0.20
    +added more Move data
    +improved visual structure of the code

--- pokestats.php
+++ pokestats.php
@@ -1,8 +1,62 @@
 <?php
 
 $stats = [
-	'Pikachu' => ['Moves'=>['Quick Attack'=>['PowerLevel'=>40, 'PowerPoints'=>30], 'Growl'=>['PowerLevel'=>45, 'PowerPoints'=>40], 'Thunder Shock'=>['PowerLevel'=>40, 'PowerPoints'=>30], 'Discharge'=>['PowerLevel'=>80, 'PowerPoints'=>15],]  ],
-	'Charmeleon' => ['Moves'=>['Ember'=>['PowerLevel'=>40, 'PowerPoints'=>25], 'Smokescreen'=>['PowerLevel'=>0, 'PowerPoints'=>20], 'Dragon Rage'=>['PowerLevel'=>0, 'PowerPoints'=>10], 'Fire Fang'=>['PowerLevel'=>65, 'PowerPoints'=>15],]  ],
+	'Pikachu' => [
+		'Moves'=>[
+			'Quick Attack'=>[
+				'PowerLevel'=>40,
+				'PowerPoints'=>30,
+				'Accuracy'=>100,
+				'Category'=>'Physical'
+			],
+			'Growl'=>[
+				'PowerLevel'=>45,
+				'PowerPoints'=>40,
+				'Accuracy'=>100,
+				'Category'=>'Status'
+			],
+			'Thunder Shock'=>[
+				'PowerLevel'=>40,
+				'PowerPoints'=>30,
+				'Accuracy'=>100,
+				'Category'=>'Special'
+			],
+			'Discharge'=>[
+				'PowerLevel'=>80,
+				'PowerPoints'=>15,
+				'Accuracy'=>100,
+				'Category'=>'Special'
+			],
+		]
+	],
+	'Charmeleon' => [
+		'Moves'=>[
+			'Ember'=>[
+				'PowerLevel'=>40,
+				'PowerPoints'=>25,
+				'Accuracy'=>100,
+				'Category'=>'Special'
+			],
+			'Smokescreen'=>[
+				'PowerLevel'=>0,
+				'PowerPoints'=>20,
+				'Accuracy'=>100,
+				'Category'=>'Status'
+			],
+			'Dragon Rage'=>[
+				'PowerLevel'=>0,
+				'PowerPoints'=>10,
+				'Accuracy'=>100,
+				'Category'=>'Special'
+			],
+			'Fire Fang'=>[
+				'PowerLevel'=>65,
+				'PowerPoints'=>15,
+				'Accuracy'=>95,
+				'Category'=>'Physical'
+			],
+		]
+	],
 ];
 
 
